ab9222d70f22ec045323f44aebcc3cadeb6ee886
minaka1412 <minaka1412@gmail.com>
2017-12-08 18:26:33 +0900

    パッケージ用ファイルを追加

---
+++ __init__.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+# pylint: disable=C0103
+"""
+ * パッケージ用ファイル
+ * Development by minaka1412
+"""
+from . import Directory
+from . import File
+from . import FileSystem
+from . import Logger
+from . import Path
+from . import System
